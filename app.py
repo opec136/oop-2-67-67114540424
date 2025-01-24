dc6feffbce70a2c1d214b27cd598fb4abfb98e7e
opec <opecsseepeang@gmail.com>
2025-01-24 14:04:01 +0700

    สร้างคบาสแรกบน Github repo 555

---
+++ app.py
@@ -0,0 +1,13 @@
+class student (obj):
+  stuber_cout = 0 
+
+def_now_(self):
+  print('student._now_')
+
+def_init_(self):
+  print('student_inti_')
+  self.gea = 4.00
+
+def student_hard(self):
+  print('sir, yes sir.')
+  
